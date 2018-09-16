cf798478cb86a3d645af04012927fd9e8694be37
Justin B <justinleighb@gmail.com>
2018-09-15 23:16:26 -0400

    Finished the Menu App with the Log exercise

--- Menu/app/src/main/java/com/example/android/menu/MainActivity.java
+++ Menu/app/src/main/java/com/example/android/menu/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android.menu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +15,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void printToLogs(View view) {
+        TextView menuItem1 = (TextView) findViewById(R.id.menu_item_1);
         // Find first menu item TextView and print the text to the logs
-
+        Log.v("MainActivity.java", menuItem1.getText().toString());
         // Find second menu item TextView and print the text to the logs
-
+        TextView menuItem2 = (TextView) findViewById(R.id.menu_item_2);
+        Log.v("MainActivity.java", menuItem2.getText().toString());
         // Find third menu item TextView and print the text to the logs
-
+        TextView menuItem3 = (TextView) findViewById(R.id.menu_item_3);
+        Log.v("MainActivity.java", menuItem3.getText().toString());
     }
 }
