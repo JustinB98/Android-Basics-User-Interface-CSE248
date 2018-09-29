9aba15d2727ce9bd165c9590706344d0e37e4f2f
Justin B <justinleighb@gmail.com>
2018-09-29 18:11:39 -0400

    Added Thursday and Friday Fragments So, as far as I can see, the way the view pager works is like this. It needs an adapter like the list view in order to get which view it needs. Now, in the adapter class, there needs to be two methods that are overrided. First, is the getCount(). This should return how many fragments, or screens, the pager will be showing. The next is the getItem(int position) method. This method provides an integer as a argument and this int will be the position the page view provides to represent which screen the user is on. It starts at 0 and goes to your getCount() number minus 1. From there, you should return a fragment that will contain the view of what you want to show. For example, the MondayFragment extends the Fragments class. Note this is the android.support.v4.app.Fragment (not sure if it makes a different because there are two of them, probably because the view pager tag in the activity_main.xml is android.support.v4.view.ViewPager). Then you need to override a method called getView. This should return the view you want to be on the screen. In this case, we want to make a view object from a layout, so we need to inflate it. We are given an inflater and the viewgroup container (plus a bundle but we don't use that). then just use the inflate method on the layoutinflater passed in and give the resource id of the layout, then the viewgroup container passed in, then false, to show that you don't want to attach it to the parent. Going to be doing more experiments to see what each argument does.

---
+++ ViewPagerExample/app/src/main/java/com/example/android/viewpager/FridayFragment.java
@@ -0,0 +1,20 @@
+package com.example.android.viewpager;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Justin on 9/29/2018.
+ */
+
+public class FridayFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_friday, container, false);
+    }
+}
