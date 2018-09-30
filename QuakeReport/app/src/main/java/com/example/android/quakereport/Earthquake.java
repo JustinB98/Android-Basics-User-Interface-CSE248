e9db5b6c02e68726d56e5b28ddf2e3cba12f2ebc
Justin B <justinleighb@gmail.com>
2018-09-30 18:23:17 -0400

    Finished the First Lesson for Networking Forgot to commit along the way but here's what happened: Parsed a sample JSON request that was hardcoded. Took the information that was in the JSON file and put that info into the earthquake app. The layout was worked along the way, but we just ended up copying and pasting some xml they gave us, which I really didn't like. I wanted to understand why we should make the layout as they did. They just said "here take this" and we're just expected to just paste it no questions ask. Nonetheless, the layout does look very nice. We even took the opportunity to create a shape in the drawable folder and set it as a background of the magnitude text view. Then we programmically changed the color of the circle using switch statements which does look pretty bad in the code. I wonder if there's a way to programmically pick the color using a formula... maybe another time. Basically, in the end, we got the app to work just great using sample code. Now, we have to learn about networking for real this time. The good news is, once we do that, we just need to replace the string for the JSON parser method once we get the JSON file from the USGS website.

--- QuakeReport/app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -6,25 +6,41 @@ package com.example.android.quakereport;
 
 public class Earthquake {
 
-    private String mMagnitude;
+    private double mMagnitude;
     private String mLocation;
-    private String mDate;
+    private long mTimeInMilliseconds;
+    private String mUrl;
 
-    public Earthquake(String magnitude, String location, String date) {
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the location where the earthquake happened
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *                           earthquake happened
+     * @param url is the website URL to find more details about the earthquake
+     */
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
         mMagnitude = magnitude;
         mLocation = location;
-        mDate = date;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
     }
 
     public String getLocation() {
         return mLocation;
     }
 
-    public String getMagnitude() {
+    public double getMagnitude() {
         return mMagnitude;
     }
 
-    public String getDate() {
-        return mDate;
+    public long getTimeInTilliseconds() {
+        return mTimeInMilliseconds;
     }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
 }
--- QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,14 +1,19 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Justin on 9/30/2018.
@@ -16,11 +21,10 @@ import java.util.ArrayList;
 
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
-    private Context mContext;
+    private static final String LOCATION_SEPARATOR = " of ";
 
     public EarthquakeAdapter(Context context, ArrayList<Earthquake> list) {
         super(context, 0, list);
-        mContext = context;
     }
 
     @NonNull
@@ -34,14 +38,115 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
         Earthquake earthquake = getItem(position);
 
-        TextView magnitudeView = (TextView) convertView.findViewById(R.id.magnitude_view);
-        magnitudeView.setText(earthquake.getMagnitude());
+        TextView magnitudeView = (TextView) convertView.findViewById(R.id.magnitude);
+        double mag = earthquake.getMagnitude();
+        String formattedMag = formatMagnitude(mag);
+        magnitudeView.setText(formattedMag);
 
-        TextView locationView = (TextView) convertView.findViewById(R.id.location_view);
-        locationView.setText(earthquake.getLocation());
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
 
-        TextView dateView = (TextView) convertView.findViewById(R.id.date_view);
-        dateView.setText(earthquake.getDate());
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(earthquake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
+        TextView locationView = (TextView) convertView.findViewById(R.id.primary_location);
+        TextView offsetView = (TextView) convertView.findViewById(R.id.location_offset);
+
+        String originalLocation = earthquake.getLocation();
+        String primaryLocation, offsetLocation;
+
+        if (originalLocation.contains(LOCATION_SEPARATOR)) {
+            String[] parts = originalLocation.split(LOCATION_SEPARATOR);
+            offsetLocation = parts[0] + LOCATION_SEPARATOR;
+            primaryLocation = parts[1];
+        } else {
+            offsetLocation = getContext().getString(R.string.near_the);
+            primaryLocation = originalLocation;
+        }
+
+        locationView.setText(primaryLocation);
+        offsetView.setText(offsetLocation);
+
+        long time = earthquake.getTimeInTilliseconds();
+        Date date = new Date(time);
+        String formattedDate = formatDate(date);
+        String formattedTime = formatTime(date);
+
+        TextView dateView = (TextView) convertView.findViewById(R.id.date);
+        TextView timeView = (TextView) convertView.findViewById(R.id.time);
+        dateView.setText(formattedDate);
+        timeView.setText(formattedTime);
+
         return convertView;
     }
+
+    private int getMagnitudeColor(double magnitude) {
+        int colorRes;
+        int flooredMag = (int) Math.floor(magnitude);
+        switch (flooredMag) {
+            case 0:
+            case 1:
+                colorRes = R.color.magnitude1;
+                break;
+            case 2:
+                colorRes = R.color.magnitude2;
+                break;
+            case 3:
+                colorRes = R.color.magnitude3;
+                break;
+            case 4:
+                colorRes = R.color.magnitude4;
+                break;
+            case 5:
+                colorRes = R.color.magnitude5;
+                break;
+            case 6:
+                colorRes = R.color.magnitude6;
+                break;
+            case 7:
+                colorRes = R.color.magnitude7;
+                break;
+            case 8:
+                colorRes = R.color.magnitude8;
+                break;
+            case 9:
+                colorRes = R.color.magnitude9;
+                break;
+            default:
+                colorRes = R.color.magnitude10plus;
+        }
+
+        return ContextCompat.getColor(getContext(), colorRes);
+
+    }
+
+    /**
+     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     */
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted magnitude string showing 1 decimal place (i.e. "3.2")
+     * from a decimal magnitude value.
+     */
+    private String formatMagnitude(double magnitude) {
+        DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
+        return magnitudeFormat.format(magnitude);
+    }
+
 }
