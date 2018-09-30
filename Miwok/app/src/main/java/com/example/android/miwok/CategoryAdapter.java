176fa87b87bb8d657029f57e1f0b80d1adb6deb9
Justin B <justinleighb@gmail.com>
2018-09-29 21:17:31 -0400

    Actually Finished the Miwok App (For Real) Put the page view and tab layout into the app and now it looks nice. Also styled the tab layout using the udacity code. Not going to lie, I really didn't understand why everything works. Going to continue to look at it for now but I really wish they had videos for further explaination. They just tell you to look at the documentation, which is very difficult to understand (for me at least). But with that, the course Multiscreen Apps is FINALLY finished.

--- Miwok/app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ Miwok/app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,8 +11,11 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fm) {
+    private Context mContext;
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -20,11 +24,27 @@ public class CategoryAdapter extends FragmentPagerAdapter {
             case 0:
                 return new NumbersFragment();
             case 1:
-                return new ColorsFragment();
-            case 2:
                 return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
             case 3:
                 return new PhrasesFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position) {
+            case 0:
+                return mContext.getString(R.string.category_numbers);
+            case 1:
+                return mContext.getString(R.string.category_family);
+            case 2:
+                return mContext.getString(R.string.category_colors);
+            case 3:
+                return mContext.getString(R.string.category_phrases);
             default:
                 return null;
         }
--- Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -28,8 +29,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        CategoryAdapter categoryAdapter = new CategoryAdapter(getSupportFragmentManager());
+        CategoryAdapter categoryAdapter = new CategoryAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(categoryAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
 
     }
 
