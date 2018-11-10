3bb7b372d3b1af22155f7eea74e234b5a5b07168
Justin B <justinleighb@gmail.com>
2018-11-10 16:36:30 -0500

    Fixed the checkbox issue The issue seems to be gone. After looking at stackoverflow, there was a comment with the same issue as me (mulitple callback calls to the checkbox setOnCheckedChangeListener) and someone suggested to remove the listener then add it back. So I removed the listeners, then set the convertView's data fields, then added the listener back, and it seems to be working. Will do more tests

--- FinancialManager/app/src/main/java/behrman/justin/financialmanager/adapters/SelectableAdapter.java
+++ FinancialManager/app/src/main/java/behrman/justin/financialmanager/adapters/SelectableAdapter.java
@@ -34,22 +34,33 @@ public class SelectableAdapter extends ArrayAdapter<Card> {
     public View getView(int position, @Nullable View convertView, ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_monthly_calculations, parent, false);
-            initOnClick(convertView);
-            initCheckBoxListener(convertView, position);
         }
+        // you have to remove the listeners so they don't get called again
+        removeListeners(convertView);
         putCardIntoViews(position, convertView);
+        // after putting the data in, then put back the listeners
+        initCheckBoxListener(convertView, position);
+        initOnClick(convertView);
         return convertView;
+    }
+
+    // https://stackoverflow.com/questions/3149414/how-to-receive-a-event-on-android-checkbox-check-change
+    // @Tatarize
+    private void removeListeners(View convertView) {
+        convertView.setOnClickListener(null);
+        CheckBox checkBox = convertView.findViewById(R.id.checkbox);
+        checkBox.setOnCheckedChangeListener(null);
     }
 
     private void putCardIntoViews(int pos, View convertView) {
         Card currentCard = getItem(pos);
-        Log.i(LOG_TAG, "card: " + currentCard + ", item checked: " + listView.isItemChecked(pos));
+        // Log.i(LOG_TAG, "card: " + currentCard + ", item checked: " + listView.isItemChecked(pos));
         TextView cardNameView = (TextView) convertView.findViewById(R.id.card_name_view);
         cardNameView.setText(currentCard.getCardName());
         TextView cardTypeView = (TextView) convertView.findViewById(R.id.card_type_view);
         cardTypeView.setText(currentCard.getCardType().toString());
         CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkbox);
-        checkBox.setChecked(convertView.isSelected());
+        checkBox.setChecked(listView.isItemChecked(pos));
     }
 
     private void initOnClick(View convertView) {
