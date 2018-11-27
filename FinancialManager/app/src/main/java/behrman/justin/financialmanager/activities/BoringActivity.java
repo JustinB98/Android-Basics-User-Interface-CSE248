fa45ac854357c9c0b4f49a0f90bdcb2cdd5085d5
Justin B <justinleighb@gmail.com>
2018-11-26 20:46:15 -0500

    Added Plaid Info Layout Plaid info layout finished and now the auto and manual card help links to the plaid info and the plaid info links to the auto and manual card help. This is probably as far as I'll go with all this. This was just for me to test some ideas I had. The Boring Activity is something I'll probably use from time to time.

--- FinancialManager/app/src/main/java/behrman/justin/financialmanager/activities/BoringActivity.java
+++ FinancialManager/app/src/main/java/behrman/justin/financialmanager/activities/BoringActivity.java
@@ -1,9 +1,13 @@
 package behrman.justin.financialmanager.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import behrman.justin.financialmanager.R;
 import behrman.justin.financialmanager.utils.StringConstants;
 
 /**
@@ -30,4 +34,32 @@ public class BoringActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(titleResId);
         }
     }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.link_to_auto_card_help:
+                finish();
+                startActivity(this, R.layout.auto_vs_manual, R.string.auto_vs_manual_title);
+                break;
+            case R.id.link_to_plaid_help:
+                finish();
+                startActivity(this, R.layout.plaid_info, R.string.plaid_info_title);
+                break;
+        }
+    }
+
+    public static void startActivity(Context context, int resId, int titleId) {
+        Intent intent = new Intent(context, BoringActivity.class);
+        intent.putExtra(StringConstants.RES_ID_KEY, resId);
+        intent.putExtra(StringConstants.TITLE_KEY, titleId);
+        context.startActivity(intent);
+    }
+
+    public static void startActivity(Context context, int layoutResId, String title) {
+        Intent intent = new Intent(context, BoringActivity.class);
+        intent.putExtra(StringConstants.RES_ID_KEY, layoutResId);
+        intent.putExtra(StringConstants.TITLE_KEY, title);
+        context.startActivity(intent);
+    }
+
 }
--- FinancialManager/app/src/main/java/behrman/justin/financialmanager/fragments/InfoFragment.java
+++ FinancialManager/app/src/main/java/behrman/justin/financialmanager/fragments/InfoFragment.java
@@ -1,6 +1,5 @@
 package behrman.justin.financialmanager.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
@@ -8,7 +7,6 @@ import android.support.v7.preference.PreferenceScreen;
 
 import behrman.justin.financialmanager.R;
 import behrman.justin.financialmanager.activities.BoringActivity;
-import behrman.justin.financialmanager.utils.StringConstants;
 
 public class InfoFragment extends PreferenceFragmentCompat {
     @Override
@@ -24,24 +22,14 @@ public class InfoFragment extends PreferenceFragmentCompat {
 
     private void checkForClicks(String key) {
         switch (key) {
-
+            case "autoVsManual":
+                BoringActivity.startActivity(getContext(), R.layout.auto_vs_manual, R.string.auto_vs_manual_title);
+                break;
+            case "plaidInfo":
+                BoringActivity.startActivity(getContext(), R.layout.plaid_info, R.string.plaid_info_title);
+                break;
         }
     }
-
-    private void openBoringActivity(int layoutResId, int titleId) {
-        Intent intent = new Intent(getContext(), BoringActivity.class);
-        intent.putExtra(StringConstants.RES_ID_KEY, layoutResId);
-        intent.putExtra(StringConstants.TITLE_KEY, titleId);
-        startActivity(intent);
-    }
-
-    private void openBoringActivity(int layoutResId, String title) {
-        Intent intent = new Intent(getContext(), BoringActivity.class);
-        intent.putExtra(StringConstants.RES_ID_KEY, layoutResId);
-        intent.putExtra(StringConstants.TITLE_KEY, title);
-        startActivity(intent);
-    }
-
     // https://stackoverflow.com/questions/18509369/android-how-to-get-remove-margin-padding-in-preference-screen/18509566
     @Override
     public void setPreferenceScreen(PreferenceScreen preferenceScreen) {
